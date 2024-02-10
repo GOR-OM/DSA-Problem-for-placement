bc70cdb7cb6a7c11bef74a8b38322c4a3a9bf18d
GOR-OM <202101484@daiict.ac.in>
2024-02-10 17:32:22 +0530

    for circular array

--- Stack & Queue/Next Greater Element.cpp
+++ Stack & Queue/Next Greater Element.cpp
@@ -23,3 +23,25 @@ vector<int> nextGreaterElement(vector<int>& arr, int n) {
     reverse(ans.begin(), ans.end()); // Reverse the answer vector
     return ans;
 }
+
+
+// Modification in question :)  (like circular array)
+
+vector<int> nextGreaterElement(vector<int>& arr, int n) {
+    vector<int> ans(n, -1);
+    stack<int> st;
+
+    for (int i = 2 * n - 1; i >= 0; i--) {
+        while (!st.empty() && arr[i % n] >= st.top()) {
+            st.pop();
+        }
+
+        if (!st.empty()) {
+            ans[i % n] = st.top();
+        }
+
+        st.push(arr[i % n]);
+    }
+
+    return ans;
+}
