2e208ffaa5e5a11d5f0a20293fdcc54d5f7da86e
GOR-OM <202101484@daiict.ac.in>
2024-05-13 16:32:01 +0530

    Bellman Ford algo

---
+++ Graph/Bellman Ford algo.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+		vector<int> dist(V, 1e8);
+		dist[S] = 0;
+		for (int i = 0; i < V - 1; i++) {
+			for (auto it : edges) {
+				int u = it[0];
+				int v = it[1];
+				int wt = it[2];
+				if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+					dist[v] = dist[u] + wt;
+				}
+			}
+		}
+		// Nth relaxation to check negative cycle
+		for (auto it : edges) {
+			int u = it[0];
+			int v = it[1];
+			int wt = it[2];
+			if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+				return { -1};
+			}
+		}
+
+
+		return dist;
+	}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> edges;
+    for (int i = 0; i < m; i++) {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        edges.push_back({u, v, wt});
+    }
+    vector<int> res = bellman_ford(n, edges, 0);
+    for (int i = 0; i < n; i++) {
+        cout << res[i] << " ";
+    }
+    return 0;
+}
