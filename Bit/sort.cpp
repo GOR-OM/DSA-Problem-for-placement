27104fd512d8bb05558e79f4b97fea2706dad1fa
GOR-OM <202101484@daiict.ac.in>
2024-05-26 15:32:35 +0530

    sorting bit by given condition

---
+++ Bit/sort.cpp
@@ -0,0 +1,21 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+
+vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(),arr.end(),[](int a,int b){
+            int count1 = __builtin_popcount(a);
+            int count2 = __builtin_popcount(b);
+            if(count1==count2){
+                return a<b;
+            }
+            return count1<count2;
+        });
+    }
